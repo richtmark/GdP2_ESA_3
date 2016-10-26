64b9983b13774664f0200b3f0a20ce1c41b069a5
Markus Richter <s48874@beuth-hochschule.de>
2016-10-26 21:13:14 +0200

    Update IntRechner.java
    bugfixing?

--- IntRechner.java
+++ IntRechner.java
@@ -6,13 +6,16 @@
 
 public class IntRechner implements Rechner {
 		
-		String[] ergebnis = new String[5];
-
 		@Override
 		public String[] rechnen(String number1, String number2) throws NumberFormatException {
+			
+		String[] ergebnis = new String[5];
 	
+		try {	
 			Integer intnumber1 = Integer.decode(number1);
+
 			Integer intnumber2 = Integer.decode(number2);
+		
 				
 			int sum = (intnumber1 + intnumber2);
 			int diff = (intnumber1 - intnumber2);
@@ -20,20 +23,21 @@ public class IntRechner implements Rechner {
 			int div = (intnumber1 / intnumber2);
 			int mod = (intnumber1 % intnumber2);
 
-			try{
+
 				ergebnis[0] = String.valueOf(sum);
 				ergebnis[1] = String.valueOf(diff);
 				ergebnis[2] = String.valueOf(prod);
 				ergebnis[3] = String.valueOf(div);
 				ergebnis[4] = String.valueOf(mod);
-
+				
+			} catch(NumberFormatException e) {
+				e.printStackTrace();
+			
 			}catch(ArithmeticException e){
-				System.out.println("Division durch Null geht nicht!");
-			}catch(NumberFormatException e){
-				System.out.println("Falsche Zeicheneingabe!");}
-			return ergebnis;
+				e.printStackTrace();
 			
-			
+			}
+		return ergebnis;
 	}
       
 }
