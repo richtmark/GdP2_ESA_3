51b51578917ada049f3ad6411048e40a0afbcba9
Markus Richter <s48874@beuth-hochschule.de>
2016-10-26 21:13:52 +0200

    Update FloatRechner.java
    bugfixing?

--- FloatRechner.java
+++ FloatRechner.java
@@ -6,11 +6,13 @@
         
 	public class FloatRechner implements Rechner {
 			
-			String[] ergebnis = new String[5];
+			
 
 			@Override
 			public String[] rechnen(String number1, String number2) throws NumberFormatException {
-		
+				
+				String[] ergebnis = new String[5];
+			try {
 				Float fnumber1 = Float.parseFloat(number1);
 				Float fnumber2 = Float.parseFloat(number2);
 					
@@ -20,7 +22,6 @@
 				Float div = (fnumber1 / fnumber2);
 				Float mod = (fnumber1 % fnumber2);
 
-				try{
 					ergebnis[0] = String.valueOf(sum);
 					ergebnis[1] = String.valueOf(diff);
 					ergebnis[2] = String.valueOf(prod);
@@ -28,12 +29,11 @@
 					ergebnis[4] = String.valueOf(mod);
 
 				}catch(ArithmeticException e){
-					System.out.println("Division durch Null geht nicht!");
+					e.printStackTrace();
 				}catch(NumberFormatException e){
-					System.out.println("Falsche Zeicheneingabe!");}
+					e.printStackTrace();	
+			}
 				return ergebnis;
-				
-				
 		}
 	      
 	}
